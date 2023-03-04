a84d0e64774ee46c568104d0d057c6e166df5dd8
Abdelrahman Emam <abdelrahmanemam498@gmail.com>
2023-03-04 15:58:06 +0200

    last

---
+++ js/main.js
@@ -0,0 +1,100 @@
+TweenMax.from(".left", 1.6, {
+    delay: 0.6,
+    width: 0,
+    ease: Expo.easeInOut
+  })
+TweenMax.from(".right",1.6,{
+    delay:0.6,
+    width:0,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".logo",1,{
+    delay:1,
+    opacity:0,
+    x:-20,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".menu",1,{
+    delay:1.2,
+    opacity:0,
+    x:-20,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".categories",1,{
+    delay:1.4,
+    opacity:0,
+    x:-150,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".search",1,{
+    delay:1.6,
+    opacity:0,
+    x:-20,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".bag",1,{
+    delay:1.6,
+    opacity:0,
+    x:-20,
+    ease:Expo.easeInOut
+})
+TweenMax.staggerfrom(".media ul li",1,{
+    delay:2,
+    opacity:0,
+    x:-20,
+    ease:Expo.easeInOut
+},0.08)
+TweenMax.from(".size",1,{
+    delay:1.8,
+    opacity:0,
+    x:-20,
+    ease:Expo.easeInOut
+})
+TweenMax.staggerfrom(".size ul li",.3,{
+    delay:2,
+    opacity:0,
+    y:20,
+    ease:Power3.easeInOut
+},0.08)
+TweenMax.staggerfrom(".dot",1,{
+    delay:2.4,
+    opacity:0,
+    x:20,
+    ease:Power3.easeInOut
+},0.08)
+TweenMax.from(".bottomnav",1,{
+    delay:2.4,
+    opacity:0,
+    x:-20,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".bottomnav ul li:fris-child",.5,{
+    delay:2.4,
+    opacity:0,
+    x:-20,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".bottomnav ul li:last-child",.6,{
+    delay:2.4,
+    opacity:0,
+    x:-20,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".product-img",3,{
+    delay:2,
+    opacity:0,
+    y:20,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".prodcut-title",3,{
+    delay:2.2,
+    opacity:0,
+    y:50,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".prodcut-subtitle",3,{
+    delay:2.4,
+    opacity:0,
+    y:50,
+    ease:Expo.easeInOut
+})
